86db39734bf8a20d09db411424887f42844579e0
Ilya Oborin <i.oborin@stmd.pro>
2024-02-17 20:39:45 +0500

    Удаляет лишние файлы

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import {AppHeader} from "./components/app-header/app-header";
 import {BurgerIngredients} from "./components/burger-ingredients/burger-ingredients";
 import {BurgerConstructor} from "./components/burger-constructor/burger-constructor";
