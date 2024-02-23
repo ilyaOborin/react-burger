bf7ab355aa6a1ab235fe682b0c058cc0ef9f3740
Ilya Oborin <i.oborin@stmd.pro>
2024-02-23 17:12:58 +0500

    Добавляет запрос на получение ингридиентов

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {AppHeader} from "./components/app-header/app-header";
 import {BurgerIngredients} from "./components/burger-ingredients/burger-ingredients";
 import {BurgerConstructor} from "./components/burger-constructor/burger-constructor";
 
+const INGREDIENTS_URL = 'https://norma.nomoreparties.space/api/ingredients';
+
 function App() {
-  return (
-      <>
-          <AppHeader/>
-          <div className="container">
-            <div className="content">
-                <BurgerIngredients />
-                <BurgerConstructor />
+    const [ingredients, setIngredients] = useState([]);
+
+    useEffect(() => {
+        fetch(INGREDIENTS_URL)
+            .then(res => {
+                if(res.ok) {
+                    return res.json()
+                } else {
+                    throw new Error("Ошибка выполнения запроса");
+                }
+            })
+            .then(data => setIngredients(data.data))
+            .catch(e => console.log(e));
+    }, [])
+
+    return (
+        <>
+            <AppHeader/>
+            <div className="container">
+                <div className="content">
+                    <BurgerIngredients ingredients={ingredients}/>
+                    <BurgerConstructor />
+                </div>
             </div>
-          </div>
-      </>
-  );
+        </>
+    );
 }
 
 export default App;
