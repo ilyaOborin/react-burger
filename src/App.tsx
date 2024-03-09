48c31bb17bdcd485657d3d6b4f139b1a80177c18
Ilya Oborin <i.oborin@stmd.pro>
2024-03-09 16:18:26 +0500

    Оборачивает проект в dnd

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,20 @@ import React, {useEffect, useState} from 'react';
 import {AppHeader} from "./components/app-header/app-header";
 import {BurgerIngredients} from "./components/burger-ingredients/burger-ingredients";
 import {BurgerConstructor} from "./components/burger-constructor/burger-constructor";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
-const INGREDIENTS_URL = 'https://norma.nomoreparties.space/api/ingredients';
 
 function App() {
-    const [ingredients, setIngredients] = useState([]);
-
-    useEffect(() => {
-        fetch(INGREDIENTS_URL)
-            .then(res => {
-                if(res.ok) {
-                    return res.json()
-                } else {
-                    throw new Error("Ошибка выполнения запроса");
-                }
-            })
-            .then(data => setIngredients(data.data))
-            .catch(e => console.log(e));
-    }, [])
-
     return (
         <>
             <AppHeader/>
             <div className="container">
                 <div className="content">
-                    <BurgerIngredients ingredients={ingredients}/>
-                    <BurgerConstructor />
+                    <DndProvider backend={HTML5Backend}>
+                        <BurgerIngredients />
+                        <BurgerConstructor />
+                    </DndProvider>
                 </div>
             </div>
         </>
