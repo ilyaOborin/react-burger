43d594f18798fa791538192dd110dc62ac36a10c
Ilya Oborin <i.oborin@stmd.pro>
2024-03-10 19:41:27 +0500

    Переносит app в папку с компонентами

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,21 @@
-import React, {useEffect, useState} from 'react';
-import {AppHeader} from "./components/app-header/app-header";
-import {BurgerIngredients} from "./components/burger-ingredients/burger-ingredients";
-import {BurgerConstructor} from "./components/burger-constructor/burger-constructor";
+import React from 'react';
+import {AppHeader} from "../app-header/app-header";
+import {BurgerIngredients} from "../burger-ingredients/burger-ingredients";
+import {BurgerConstructor} from "../burger-constructor/burger-constructor";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-
 
 function App() {
     return (
         <>
             <AppHeader/>
             <div className="container">
-                <div className="content">
+                <main className="content">
                     <DndProvider backend={HTML5Backend}>
                         <BurgerIngredients />
                         <BurgerConstructor />
                     </DndProvider>
-                </div>
+                </main>
             </div>
         </>
     );
